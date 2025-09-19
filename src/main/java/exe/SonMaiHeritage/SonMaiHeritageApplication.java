6dd1dcafb79e28126e1bbc7b4d4890441456d09f
TuNA <atoodoo04@gmail.com>
2025-09-19 10:53:16 +0700

    initialize project

---
+++ src/main/java/exe/SonMaiHeritage/SonMaiHeritageApplication.java
@@ -0,0 +1,13 @@
+package exe.SonMaiHeritage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonMaiHeritageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonMaiHeritageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exe/SonMaiHeritage/SonMaiHeritageApplicationTests.java
@@ -0,0 +1,13 @@
+package exe.SonMaiHeritage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonMaiHeritageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
